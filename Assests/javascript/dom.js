176dc5e7c650769ce9ea99e189a98974df7cd333
marcos <marcosdasilva399@gmail.com>
2025-04-06 13:43:24 -0300

    update functions and play sound

--- Assests/javascript/dom.js
+++ Assests/javascript/dom.js
@@ -1,3 +1,2 @@
 window.addEventListener('resize', verifyResolution);
-
 verifyResolution();
--- Assests/javascript/functions.js
+++ Assests/javascript/functions.js
@@ -1,97 +1,91 @@
 let maxL = 36;
 let keys = document.querySelector('.bkeys')
 let sounds = document.querySelector('.sounds');
-let keyboardAC = [{whiteKey:'1',blackKey:'!'},{whiteKey:'2',blackKey:'@'},{whiteKey:'3'},{whiteKey:'4',blackKey:'$'},{whiteKey:'5',blackKey:'%'},{whiteKey:'6',blackKey:'¨'}
-    ,{whiteKey:'7'},{whiteKey:'8',blackKey:'^'},{whiteKey:'9',blackKey:'('},{whiteKey:'0'},{whiteKey:'q',blackKey:'Q'},{whiteKey:'w',blackKey:'W'},{whiteKey:'e',blackKey:'E'}
+let audioMap = {};
+let keyboardAC = [{whiteKey:'1',blackKey:'!'},{whiteKey:'2',blackKey:'@'},{whiteKey:'3'},{whiteKey:'4',blackKey:'$'},{whiteKey:'5',blackKey:'%'},{whiteKey:'6',blackKey:'&'}
+    ,{whiteKey:'7'},{whiteKey:'8',blackKey:'('},{whiteKey:'9',blackKey:')'},{whiteKey:'0'},{whiteKey:'q',blackKey:'Q'},{whiteKey:'w',blackKey:'W'},{whiteKey:'e',blackKey:'E'}
     ,{whiteKey:'r'},{whiteKey:'t',blackKey:'T'},{whiteKey:'y',blackKey:'Y'},{whiteKey:'u'},{whiteKey:'i',blackKey:'I'},{whiteKey:'o',blackKey:'O'},{whiteKey:'p',blackKey:'P'}
     ,{whiteKey:'a'},{whiteKey:'s',blackKey:'S'},{whiteKey:'d',blackKey:'D'},{whiteKey:'f'},{whiteKey:'g',blackKey:'G'},{whiteKey:'h',blackKey:'H'},{whiteKey:'j',blackKey:'J'}
     ,{whiteKey:'k'},{whiteKey:'l',blackKey:'L'},{whiteKey:'z',blackKey:'Z'},{whiteKey:'x'},{whiteKey:'c',blackKey:'C'},{whiteKey:'v',blackKey:'V'},{whiteKey:'b',blackKey:'B'}
     ,{whiteKey:'n'},{whiteKey:'m'}];
 
+function update(){
+    verifyResolution();
+}
+
 function genereteKeys(){
     keys.innerHTML = '';
-    let count = 0;
-    let countA = 1;
-    for(i = 0; i < maxL; i++){
-    
+    const visibleKeys = keyboardAC.slice(0, maxL);
+    visibleKeys.forEach(key =>{
+            
         let newKeys = document.createElement('div');
         newKeys.setAttribute('class','keys');
         keys.append(newKeys);
-        let newKeyW = document.createElement('div');
-        newKeyW.setAttribute('class','white-key');
-        newKeys.append(newKeyW);
-        newKeyW.innerText = keyboardAC[i].whiteKey;
-        if(count<3 && countA === 2 && i < maxL-1){
+        if(key.whiteKey){
+            let newKeyW = document.createElement('div');
+            newKeyW.setAttribute('class','white-key');
+            newKeyW.setAttribute('data-key',`${key.whiteKey}`);
+            newKeys.append(newKeyW);
+            newKeyW.innerText = key.whiteKey;
+        }
+        if(key.blackKey){
             let newKeyB = document.createElement('div');
             newKeyB.setAttribute('class','black-key');
+            newKeyB.setAttribute('data-key',`${key.blackKey}`);
             newKeys.append(newKeyB);
-            newKeyB.innerText = keyboardAC[i].blackKey;
-            count++;
-        }else if(count<2  && countA === 1 && i < maxL-1){
-                let newKeyB = document.createElement('div');
-                newKeyB.setAttribute('class','black-key');
-                newKeys.append(newKeyB);
-                newKeyB.innerText = keyboardAC[i].blackKey;
-                count++;
-        }else if(count ===3 && countA === 2){
-            countA = 1;
-            count = 0;
-        }else{
-            countA++;
-            count = 0;
+            newKeyB.innerText = key.blackKey;
         }
-    }
+    })
 }
-
-function assignAudio(){
-    sounds.innerHTML = '';
-    let count = 0;
-    let countA = 1;
-    for(i = 0; i < maxL; i++){
-        let newAudioW = document.createElement('audio');
-        newAudioW.setAttribute('id','s_key'+keyboardAC[i].whiteKey);
-        newAudioW.setAttribute('src',`./Assests/sounds/WKey_${keyboardAC[i].whiteKey}.wav`)
-        sounds.append(newAudioW);
-        if(count<3 && countA === 2 && i < maxL-1){
-            let newAudioB = document.createElement('audio');
-            newAudioB.setAttribute('id','s_key'+keyboardAC[i].blackKey);
-            newAudioB.setAttribute('src',`./Assests/sounds/BKey_${keyboardAC[i].blackKey}.wav`);
-            sounds.append(newAudioB);
-            count++;
-        }else if(count<2  && countA === 1 && i < maxL-1){
-            let newAudioB = document.createElement('audio');
-            newAudioB.setAttribute('id','s_key'+keyboardAC[i].blackKey);
-            newAudioB.setAttribute('src',`./Assests/sounds/BKey_${keyboardAC[i].blackKey}.wav`);
-            sounds.append(newAudioB);
-            count++;
-        }else if(count ===3 && countA === 2){
-            countA = 1;
-            count = 0;
-        }else{
-            countA++;
-            count = 0;
-        }
-    }
-}
+    
 function verifyResolution(){
-    let resolution = window.innerWidth;
-    let composerInput = document.querySelector('#composerInput');
-    if(resolution >= 1800){
-        maxL = 36;
-        composerInput.style.width = '1420px';
-    }else if(resolution < 1800 && resolution > 1400){
-       maxL = 28;
-       composerInput.style.width = '1090px';
-    }else if(resolution <= 1400 && resolution > 1080){
-        maxL = 21;
-        composerInput.style.width = '800px';
-    }else if(resolution <= 1080 && resolution > 700){
-        maxL = 14;
-        composerInput.style.width = '490px';
-    }else{
-        maxL = 8;
-        composerInput.style.width = '250px';
-    }
-    genereteKeys();
-    assignAudio();
+        let resolution = window.innerWidth;
+        let composerInput = document.querySelector('#composerInput');
+        if(resolution >= 1800){
+            maxL = 36;
+            composerInput.style.width = '1420px';
+        }else if(resolution < 1800 && resolution > 1400){
+           maxL = 28;
+           composerInput.style.width = '1090px';
+        }else if(resolution <= 1400 && resolution > 1080){
+            maxL = 21;
+            composerInput.style.width = '800px';
+        }else if(resolution <= 1080 && resolution > 700){
+            maxL = 14;
+            composerInput.style.width = '490px';
+        }else{
+            maxL = 8;
+            composerInput.style.width = '250px';
+        }
+        genereteKeys();
+        preloadSounds();
 }
+function preloadSounds() {
+    audioMap = {};
+    const visibleKeys = keyboardAC.slice(0, maxL);
+    visibleKeys.forEach(key => {
+      if (key.whiteKey) {
+        audioMap[key.whiteKey] = new Audio(`./Assests/sounds/WKey_${key.whiteKey}.wav`);
+      }
+      if (key.blackKey) {
+        audioMap[key.blackKey] = new Audio(`./Assests/sounds/BKey_${key.blackKey}.wav`);
+      }
+    });
+  }
+  
+  function playSound(sound) {
+    let originalAudio = audioMap[sound];
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+    console.log(sound);
+    console.log(keyElement);
+    if (originalAudio) {
+      const audioClone = originalAudio.cloneNode();
+      audioClone.play();
+    }
+    if (keyElement){
+        keyElement.classList.add('active');
+        setTimeout(function(){
+            keyElement.classList.remove('active');
+        },300);
+    }
+  }
